e0be88aaab2bc91a3efb2275a1a9bf955aacb982
Adeeb Abubacker <adeebabu655@gmail.com>
2024-02-22 00:39:30 +0530

    initial commit

--- app.js
+++ app.js
@@ -10,12 +10,34 @@ app.use(express.json());
 
 app.get('/api/v1/crypto', async (req, res) => {
     try {
+     
+        const cryptos = await cryptoModel.find({}).sort({ fluctuation: 1 }); 
+        res.status(200).json({ cryptos })
+    } catch (e) {
+        res.status(500).json({ msg: e })
+    }
+});
+
+//sort list based on fluctuations in ascending
+app.get('/api/v1/crypto/topgainers', async (req, res) => {
+    try {
         const crypto = await cryptoModel.find({})
         res.status(200).json({ crypto })
     } catch (e) {
         res.status(500).json({ msg: e })
     }
 });
+
+//sort list based on fluctuations in descending
+app.get('/api/v1/crypto/toplosers', async (req, res) => {
+    try {
+        const crypto = await cryptoModel.find({})
+        res.status(200).json({ crypto })
+    } catch (e) {
+        res.status(500).json({ msg: e })
+    }
+});
+
 
 app.post('/api/v1/crypto', async (req, res) => {
     try {
