16d46a86d786efba91c1d3c373633ff77c7ccf8c
Adeeb Abubacker <adeebabu655@gmail.com>
2024-02-21 22:21:10 +0530

    initial commit

--- lib/core/models/failure/mainfailure.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-part 'mainfailure.freezed.dart';
-
-@freezed
-class MainFailure with _$MainFailure{
-  const factory MainFailure.clientFailure({String? message}) = ClientFailure;
-  const factory MainFailure.serverFailure() = ServerFailure;
-}
